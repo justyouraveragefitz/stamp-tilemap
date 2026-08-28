794b73f64951fdef4bcb8fb2390704cea577ec6e
Fitz <aloiajoseph656@gmail.com>
2026-08-27 21:19:35 -0400

    Add files via upload

---
+++ main-test-minimal.ts
@@ -0,0 +1,11 @@
+//% color="#6b5b95" icon="\uf00a"
+namespace tilemapStamp {
+    /**
+     * A trivial test block used only to check whether custom extension
+     * categories are showing up in the toolbox at all.
+     */
+    //% block
+    export function testBlock(): void {
+        game.splash("It works!");
+    }
+}
